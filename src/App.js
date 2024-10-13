77720d2719765c0c409e16b42012e2406164e5e4
GABRIEL CARMONA PALOP <gacapa@floridauniversitaria.es>
2024-10-13 14:08:23 +0200

    Ex5

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import {PaperProvider, Text, TextInput} from "react-native-paper";
+import React, { useState } from 'react';
+import { PaperProvider, Text, TextInput } from 'react-native-paper';
 
 const estils = {
-  text:{
-  color: 'red',
-  fontSize: 25,
-  }
-}
-const dades = () =>{
-  return(
-    <TextInput
-      label="Email"
-      value=""
-      onChangeText={text => setText(text)}
-    />
-  )
-}
-const nom = (textAMostrar)=>{
-  return(
+  text: {
+    color: 'red',
+    fontSize: 25,
+  },
+};
+
+const dades = (fieldsArray) => {
+  return (
+    fieldsArray.map((field, index) => (
+      <TextInput
+        key={index}
+        label={field}
+        value=""
+        onChangeText={() => {}}
+      />
+    ))
+  );
+};
+
+const nom = (textAMostrar) => {
+  return (
     <Text style={estils.text}>{textAMostrar}</Text>
   );
-}
+};
+
 const App = () => {
-return (
-<PaperProvider>
-  {nom("Gabriel Carmona")}
-  {dades()}
-</PaperProvider>
-);
-}
+  const fields = ['Email', 'Nom'];
+
+  return (
+    <PaperProvider>
+      {nom("Gabriel Carmona")}
+      {dades(fields)}
+    </PaperProvider>
+  );
+};
+
 export default App;
